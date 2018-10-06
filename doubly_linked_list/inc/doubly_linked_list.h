27be7d8147bfa88920ee6ddbfdfed5c7ac880b17
Kamil Kielbasa <kamkie96@gmail.com>
2018-10-06 19:53:05 +0200

    Add stub cmake structure for future implementation.

---
+++ doubly_linked_list/inc/doubly_linked_list.h
@@ -0,0 +1,4 @@
+#ifndef __DOUBLY_LINKED_LIST_H__
+#define __DOUBLY_LINKED_LIST_H__
+
+#endif /* __DOUBLY_LINKED_LIST_H__ */
---
+++ doubly_linked_list/tests/doubly_linked_list_tests.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <doubly_linked_list.h>
+
+int main(void)
+{
+	return 0;
+}
