be0ad0380eb7969b8389c8539aa7e0b87a6afa25
Kamil Kielbasa <kamkie96@gmail.com>
2018-10-29 20:46:49 +0100

    Added:  - implementation of init, push, isEmpty, isFull funcionts.    None of them were tested. There are few warning that has    to be solved.

--- stack/inc/stack.h
+++ stack/inc/stack.h
@@ -1,4 +1,22 @@
 #ifndef __STACK_H__
 #define __STACK_H__
 
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct stack_t {
+    void* __stackArray_p__;
+    int32_t __top__;
+    size_t __capacity__;
+    size_t __elementSize__;
+} stack_t;
+
+stack_t* init(size_t elementSize, size_t capacity);
+int push(stack_t* stack_p, const void* dataToAdd_p);
+int isFull(const stack_t* const stack_p);
+int isEmpty(const stack_t* const stack_p);
+/* enable to expand stack double times.
+   question is, how to write it well ? */
+int expandStack(stack_t* stack_p);
+
 #endif /* __STACK_H__ */
--- stack/src/stack.c
+++ stack/src/stack.c
@@ -1,1 +1,66 @@
 #include <stack.h>
+#include <stdlib.h>
+#include <string.h>
+
+stack_t *init(size_t elementSize, size_t capacity)
+{
+    if (elementSize == 0 || capacity == 0)
+    {
+        return NULL;
+    }
+
+    stack_t* newStack_p = (stack_t*)malloc(sizeof(*newStack_p));
+
+    if (!newStack_p)
+    {
+        return NULL;
+    }
+
+    newStack_p->__stackArray_p__ = malloc(elementSize * capacity);
+
+    if (!newStack_p->__stackArray_p__)
+    {
+        free(newStack_p);
+        return NULL;
+    }
+
+    newStack_p->__elementSize__ = elementSize;
+    newStack_p->__capacity__ = capacity;
+    newStack_p->__top__ = -1;
+    return newStack_p;
+}
+
+int isFull(const stack_t* const stack_p)
+{
+    if (!stack_p)
+    {
+        return -1;
+    }
+    return stack_p->__top__ == stack_p->__capacity__ - 1 ? 0 : -1;
+}
+
+int isEmpty(const stack_t* const stack_p)
+{
+    if (!stack_p)
+    {
+        return -1;
+    }
+    return stack_p->__top__ == -1 ? 0 : -1;
+}
+
+int push(stack_t *stack_p, const void *dataToAdd_p)
+{
+    if (!stack_p || !dataToAdd_p)
+    {
+        return -1;
+    }
+
+    /* what if stack is full ? */
+
+    stack_p->__top__++;
+    void* destAddr_p = (char*)stack_p->__stackArray_p__ +
+                       (stack_p->__top__ * stack_p->__elementSize__);
+    (void)memcpy(destAddr_p, dataToAdd_p, stack_p->__elementSize__);
+    return 0;
+}
+
